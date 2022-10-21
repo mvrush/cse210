427a32000b05832b38b0848521ea0be1c533a0f7
mvrush <mvrush@gmail.com>
2022-10-21 09:01:45 -0600

    update director

---
+++ W06/jumper/game/director.py
@@ -0,0 +1,65 @@
+from game.terminal_service import TerminalService
+from game.parachute import Parachute
+from game.word import Word
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        parachute (Parachute): The parachute that disappears as a result of missed guesses.
+        is_playing (boolean): Whether or not to keep playing.
+        word (Word): The hidden word which the user is trying to guess.
+        terminal_service: For getting and displaying information on the terminal.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+        self._parachute = Parachute() # ATTRIBUTE calls the Parachute() class and creates an instance of it called self._hider.
+        self._is_playing = True # ATTRIBUTE sets the boolean 'True' to show we are currently playing
+        self._word = Word() # ATTRIBUTE calls the Word() class and creates an instance of it called self._word.
+        self._terminal_service = TerminalService() # ATTRIBUTE calls the TerminalService() class and creates an instance of TerminalService() and assigns it to self._terminal_service
+    
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._is_playing: # says, while 'self_is_playing' is True, keep running this loop.
+            self._get_inputs() # 'get_inputs()' method. Gets user input from TerminalService()
+            self._do_updates() # 'do_updates()' method. Advances the game one step
+            self._do_outputs() # 'do_outputs()' method. Sends output to TerminalService() do display to user
+
+    def _get_inputs(self): # This is a private (_) METHOD held in the Director class
+        """Gets input from the user.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        letter_guess = self._terminal_service.read_letter() # 'read_letter()' can have prompts to ask user for letter input.
+        self._word.guessed_letter(letter_guess) # calls the 'guessed_letter()' function from the Word() class held in 'self._word' instance. Passes the guessed letter to that function
+
+    def _do_updates(self):
+        """Keeps watch on if the word is guessed an updates the parachute.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._word.correct_guess(self._parachute) # This will call the 'correct_guess()' function from the Word() class and give it access to the Parachute() class by passing  'self._parachute' so that it can send the True or False value to the Parachute() class
+
+    def _do_outputs(self):
+        """Provides a hint for the seeker to use.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        parachute = self._parachute.erase_chute() # Calls the 'draw_chute()' function from the Parachute() class
+        self._terminal_service.write_text(parachute) # Uses the TerminalService() class and passes the variable value 'parachute' to the 'write_text()' Method function to draw the parachute
+
--- W06/jumper/game/parachute.py
+++ W06/jumper/game/parachute.py
@@ -41,8 +41,8 @@ class Parachute:
             print(i)
     
     
-    def erase_chute(self, answer): # will receive 'answer' data from the Word class                
-        if answer == True:
+    def erase_chute(self, answer): # will receive 'answer' data from the Word class 
+        if answer == True:           
             for i in self._chute:
                 print(i)
             for i in self._man:
@@ -66,7 +66,15 @@ class Parachute:
 
     """ THIS IS THE FUNCTION I USED FOR TESTING """
     def erase_chute_test(self):
+        for i in self._chute: # This loops through the list and prints the item in each index position to print chute.
+            print(i)   
+
+        for i in self._man: # This loops through the list and prints the item in each index position to print man.
+            print(i)
+        
         valid_input = True
+        # As long as 'valid_input' remains true, the loop runs. It only turns to false if user inputs anything other than 'y' or 'n' OR
+        # The loop runs until the last 'if' statement becomes true and is sets 'valid_input' to False
         while valid_input:
             answer = input("Wanna live jumper? Enter y or n: ")
             if answer == 'y' or answer == 'n':
@@ -96,7 +104,8 @@ class Parachute:
                     for i in self._man:
                         print(i)
                     print(f"You're dead!\n") # the \n adds a new blank line after this is printed. It's for formatting purposes to make it look better in the terminal.
-                    break # This 'break' statement takes effect only when this 'if' statement is reached and true. It ends the program.
+                    valid_input = False # This changes 'valid_input' to False to end the loop.
+                    #break # This 'break' statement takes effect only when this 'if' statement is reached and true. It ends the program. It's another way to end the loop
                         
             else:
                 print("Invalid input, please try again") # This else statement works with the input validation. When the answer is not 'y' or 'n' valid_input becomes false and this is printed and the loop runs again.
@@ -106,7 +115,7 @@ class Parachute:
 
 """ Following lines for testing purposes. Uncomment and run parachute.py to test. """
 parachute = Parachute() # creates an instance of parachute using the 'Parachute()' class
-parachute.draw_chute() # uses the 'draw_chute()' Method (function) to draw the parachute
-parachute.draw_man() # uses the 'draw_man()' Method (function) to draw the man.
+#parachute.draw_chute() # uses the 'draw_chute()' Method (function) to draw the parachute
+#parachute.draw_man() # uses the 'draw_man()' Method (function) to draw the man.
 parachute.erase_chute_test()
     
