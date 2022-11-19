efd230fc7cb26dc44750a9bbebe17433c4046e4b
mvrush <mvrush@gmail.com>
2022-11-18 22:11:44 -0700

    trying to adjust point to fix collisions

--- W10/cycle/cycle/game/shared/point.py
+++ W10/cycle/cycle/game/shared/point.py
@@ -42,6 +42,7 @@ class Point:
             boolean: True if both x and y are equal; false if otherwise.
         """
         return self._x == other.get_x() and self._y == other.get_y()
+        #return self._x == (other.get_x() + 3) or (other.get_x() - 3) and self._y == (other.get_y() + 3) or (other.get_y() - 3)
 
     def get_x(self):
         """Gets the horizontal distance.
