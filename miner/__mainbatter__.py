0ef6a0c2e0171b4e1c3234fd060c842df0f001fc
mvrush <mvrush@gmail.com>
2022-12-05 19:58:13 -0700

    tried to get batter to work while merged, failed

--- miner/__mainbatter__.py
+++ miner/__mainbatter__.py
@@ -2,10 +2,13 @@ from constants import *
 from game.directing.director import Director
 from game.directing.scene_manager import SceneManager
 
+from game.services.keyboard_service import KeyboardService # Had to import the keyboard service to send it from here to the RFK merged director
 
+keyboard_service = KeyboardService(CELL_SIZE)
+cast = "" # I have to define cast so we can pass it below
 def main():
-    director = Director(SceneManager.VIDEO_SERVICE)
-    director.start_game()
+    director = Director(SceneManager.VIDEO_SERVICE, keyboard_service) # Here I am passing both the VIDEO_SERVICE and KEYBOARD_SERVICE variables from SceneManager
+    director.start_game(cast) # We have to pass 'cast' because I'm using the RFK director merged with Batter. If I don't pass it, the game crashes.
 
 if __name__ == "__main__":
     main()
