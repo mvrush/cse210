bcd5a671ec655204aabf66e1a1da57cc3e5dd0d5
mvrush <mvrush@gmail.com>
2022-12-08 14:57:13 -0700

    change images

--- batter-complete/batter/game/scripting/draw_racket_action.py
+++ batter-complete/batter/game/scripting/draw_racket_action.py
@@ -15,7 +15,7 @@ class DrawRacketAction(Action):
             rectangle = body.get_rectangle()
             self._video_service.draw_rectangle(rectangle, PURPLE)
             
-        animation = racket.get_animation()
-        image = animation.next_image()
+        animation = racket.get_animation() # Had to remove this to use single image.
+        image = animation.next_image() # Had to rewrite this to get a single image
         position = body.get_position()
         self._video_service.draw_image(image, position)
